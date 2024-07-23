fa1c6f179f150f1cb04060b832547a15a9e64465
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-07-24 01:57:29 +0700

    Внесение изменений

--- homework/src/App.tsx
+++ homework/src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 import ToggleButton from './components/Button'
 import ColorInput from './components/Input'
 
+export interface BlockTemplate{
+  time: number,
+  color: string
+}
+
 function App() {
 
-
+  const MassiveBlocks : BlockTemplate[] = [];
+  const [colorText, setColorText] = useState("");
   return (
     <>
       <div className="block">
         <ToggleButton/>
-        <ColorInput/>
+        <ColorInput color={colorText}/>
         <div className='container container__playground'>
 
         </div>
--- homework/src/components/Input.tsx
+++ homework/src/components/Input.tsx
@@ -1,8 +1,19 @@
+//import { useState } from 'react';
+import { useEffect } from 'react';
 import './Input.css';
-function ColorInput(){
+function ColorInput(props: { color: string | readonly string[] | undefined; }){
+
+    //Двухсторонняя привязка?
+    //const [textColor, setTextColor] = useState("");
+    useEffect(()=>{
+        if(props.color){
+            alert("Тектовое поле не может быть пустым")
+        }
+    }, [props.color])
     return(
         <div className='color__form'>
             <p>Выберите цвет</p>
+            <input value={props.color} type="text" className="form-control"></input>
             <input id='color' type='color' className="form-control"></input>
         </div>
     )
